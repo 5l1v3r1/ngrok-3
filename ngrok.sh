6fd5506d898c065f9d33028e62935c19fcb0b974
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2018-05-06 02:53:30 +0700

    Add files via upload

---
+++ ngrok.sh
@@ -0,0 +1,25 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg update && pkg upgrade
+pkg install wget
+pkg install zip
+echo
+echo "ngrok is downloading......"
+echo
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+echo
+echo "extracting file ........"
+echo
+unzip ngrok-stable-linux-arm.zip
+echo
+echo " hapus fie zip...... "
+echo
+rm ngrok-stable-linux-arm.zip
+echo
+echo
+echo
+echo  " ====================================== "
+echo    subscribe & like my channel youtube
+echo      regard,,boychongzen aka x-root
+echo  " ====================================== "
+echo
